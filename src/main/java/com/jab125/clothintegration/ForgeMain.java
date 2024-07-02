bebe4fa691aa09fd000f498670fd5d625e070e6a
Jab125 <67534807+Jab125@users.noreply.github.com>
2024-07-02 16:28:56 +1000

    Actually fix NeoForge 1.21

--- src/main/java/com/jab125/clothintegration/ForgeMain.java
+++ src/main/java/com/jab125/clothintegration/ForgeMain.java
@@ -3,21 +3,33 @@
 //$$
 //$$ import com.jab125.clothintegration.platform.PlatformUtil;
 //$$ import net.minecraftforge.api.distmarker.Dist;
+//$$ import net.minecraftforge.eventbus.api.IEventBus;
 //$$ import net.minecraftforge.fml.common.Mod;
+//$$ import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
 //$$ import net.minecraftforge.fml.loading.FMLEnvironment;
 //$$ import java.util.function.Supplier;
-//$$
 //$$ @Mod("roughlyenoughconfigscreens")
 //$$ public class ForgeMain {
 //$$
-//$$     public ForgeMain() {
-        //#if LOADER==NEO
-        //$$ PlatformUtil.assertNeoForge();
-        //#endif
-//$$         Supplier<Supplier<Runnable>> supplier = () -> () -> ConfigIntegration::init;
-//$$         if (FMLEnvironment.dist == Dist.CLIENT) {
-//$$             supplier.get().get().run();
-//$$         }
-//$$     }
+	//#if LOADER == NEO && MC >= 1.21
+//$$ 	public ForgeMain(IEventBus bus) {
+	//#else
+//$$ 	public ForgeMain() {
+//$$ 		IEventBus bus = net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext.get().getModEventBus();
+	//#endif
+		//#if LOADER==NEO
+//$$ 		PlatformUtil.assertNeoForge();
+		//#endif
+//$$ 		Supplier<Supplier<Runnable>> runnable = () -> () -> () -> {
+//$$ 			bus.addListener(this::loaded);
+//$$ 			// ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, TestConfig.CLIENT_SPEC);
+//$$ 		};
+//$$ 		if (FMLEnvironment.dist == Dist.CLIENT) runnable.get().get().run();
+//$$ 	}
+//$$
+//$$ 	private void loaded(FMLLoadCompleteEvent event) {
+//$$ 		Supplier<Supplier<Runnable>> runnable = () -> () -> () -> event.enqueueWork(ConfigIntegration::init);
+//$$ 		if (FMLEnvironment.dist == Dist.CLIENT) runnable.get().get().run();
+//$$ 	}
 //$$ }
 //#endif
